8a41eff54e38336625b9f142375dff5a056d62a9
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-01-07 20:05:59 -0300

    feat: delete storyboard and start ViewController at Scene Delegate

---
+++ FastFeet/FastFeet/Controller/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FastFeet
+//
+//  Created by Grazi  Berti on 07/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPurple
+    }
+}
+
